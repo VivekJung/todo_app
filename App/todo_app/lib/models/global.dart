836081c2c4f5d6bad394f6186236240d8a0e3bbd
VivekJung <vivekjunghamal@gmail.com>
2022-05-25 14:19:27 +0545

    tab design complete

---
+++ App/todo_app/lib/models/global.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+Color cardColor = const Color(0xFFE33E57);
+Color darkGreyColor = const Color.fromARGB(255, 0, 0, 0);
+Color whiteColor = const Color.fromRGBO(255, 255, 255, 1);
+
+TextStyle intrayTitleStyle = TextStyle(
+  fontFamily: "Avenir",
+  fontWeight: FontWeight.bold,
+  fontSize: 50,
+  color: whiteColor,
+);
